544ca1c1c465c9347ad6fb6067625c1dac3fb3dd
ZaidUTA <zaid.issa@mavs.uta.edu>
2025-11-11 20:07:05 -0600

    Python file now makes inserts for students

--- Fake.py
+++ Fake.py
@@ -1,6 +1,7 @@
 from faker import Faker
 
 fake = Faker()
+college_id_List = []
 #Creating values for Fall25_S0003_T1_College
 with open("TestInsert.sql", "w") as f:
     for _ in range(50):
@@ -10,7 +11,7 @@ with open("TestInsert.sql", "w") as f:
         name_stripped = name.replace(" ", "")
         email = name_stripped+"@edu.com"
         #email = fake.email()
-
+        college_id_List.append(college_id)
         insert_stmt = (
             f"INSERT INTO Fall25_S0003_T1_College "
             f"(Name, College_ID, Phone, Email) "
@@ -19,3 +20,19 @@ with open("TestInsert.sql", "w") as f:
         f.write(insert_stmt)
     f.close()
 
+with open("TestInsert.sql","+a") as f:
+    for _ in range(50):
+        name = fake.name().replace("'", "''")
+        name_stripped = name.replace(" ", ".")
+        email = name_stripped+"@edu.com"
+        college_id = college_id_List[_]
+        student_id = str(fake.random_int(min=1, max=99999999)).zfill(8)
+        acad_status = fake.random_element(elements=("Associate", "Bachelor", "Masters", "PHD"))
+        Major = fake.random_element(elements=("CS","Biology","Bussiness","History","Art","Lawyer","Nurse",""))
+
+        insert_stmt = (
+            f"INSERT INTO FALL25_S0003_T1_Student"
+            f"(Name,Email,College_ID,Student_ID,Acad_Status,Major)"
+            f"VALUES ('{name}','{email}','{college_id}','{student_id}','{acad_status}','{Major}');\n"
+        )
+        f.write(insert_stmt)
