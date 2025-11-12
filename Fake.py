f13ffe2c34ea75f4f6862fb7a8249dddfbfa4493
ZaidUTA <zaid.issa@mavs.uta.edu>
2025-11-11 19:29:16 -0600

    Updated Fake to now create Insert statements locally

--- Fake.py
+++ Fake.py
@@ -1,22 +1,21 @@
 from faker import Faker
-import oracledb
 
-try:
-    fake = Faker()
-    connection = oracledb.connect(user='',password='',dsn='')
-    cursor = connection.cursor()
+fake = Faker()
+#Creating values for Fall25_S0003_T1_College
+with open("TestInsert.sql", "w") as f:
+    for _ in range(50):
+        name = fake.city().replace("'", "''") + " University"
+        college_id = str(fake.random_int(min=1, max=99999999)).zfill(8)
+        phone = fake.msisdn()[:10]  # ensures numeric format, 12 digits max
+        name_stripped = name.replace(" ", "")
+        email = name_stripped+"@edu.com"
+        #email = fake.email()
 
-    for _ in range(1):
-        cursor.execute("""INSERT INTO Fall25_S0003_T1_Student(
-                       Name, Email, College_ID, Student_ID, Acad_status, Major)
-                       VALUES (:1,:2,:3,:4,:5,:6)""",(fake.name(),
-                                                      fake.email(),
-                                                      fake.random_number(digits=8),
-                                                      fake.random_number(digits=8),
-                                                      fake.random_element(elements=('Active', 'Inactive')),
-                                                      fake.random_element(elements=('CS', 'Math','Biology'))))
-        
-        connection.commit()
-        connection.close()
-except oracledb.Error as e:
-    print(f"Error connecting to Oracle Database: {e}")
+        insert_stmt = (
+            f"INSERT INTO Fall25_S0003_T1_College "
+            f"(Name, College_ID, Phone, Email) "
+            f"VALUES ('{name}', '{college_id}', '{phone}', '{email}');\n"
+        )
+        f.write(insert_stmt)
+    f.close()
+
